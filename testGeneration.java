0d5491145d8e0d350da8f0260d7cb84f95daa130
connor-callan28 <connor.callan28@yahoo.com>
2022-10-03 23:07:14 -0500

    Update testGeneration.java

--- testGeneration.java
+++ testGeneration.java
@@ -2,7 +2,7 @@ public class testGeneration {
   public static int[] randomItemList(int size){
       int result[] = new int[size];
       for(int i = 0; i<size;i++){
-          result[i] = (int)(Math.random() * size);
+          result[i] = (int)(Math.random() * (22-1+1))+1;
       }
       return result;
   }
@@ -16,7 +16,7 @@ public class testGeneration {
   }
 
   public static int addRandomProductToDatabase(dbConnection db){
-     String items[] = {"Lemon Chicken","Gyro Meat","Falafel","Onions","Cauliflower","Peppers",
+     String items[] = {"Rice Pilaf, White Rice, Buttered Chicken, Lemon Chicken","Gyro Meat","Falafel","Onions","Cauliflower","Peppers",
               "Olives","Couscous","Slaw","Tomatoes","Cucumbers","Hummus","Jalepeno Feta Dressing",
               "Vinagrette Dressing","Tahini Dressing","Yogurt Dill Dressing","Spicy Hummus",
               "Tzatziki Sauce","Harissa Yogurt"};
