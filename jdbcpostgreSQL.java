7421accf75f1f4e3bea571efa61c78a90c824288
connor-callan28 <connor.callan28@yahoo.com>
2022-10-04 12:22:18 -0500

    Create jdbcpostgreSQL.java

--- jdbcpostgreSQL.java
+++ jdbcpostgreSQL.java
@@ -16,21 +16,25 @@ public class jdbcpostgreSQL {
 
     //MAKE SURE YOU ARE ON VPN or TAMU WIFI TO ACCESS DATABASE
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         dbConnection db = new dbConnection();
         String[] cmds = {
                 "SELECT SUM(total) FROM orders", // total sales
                 "SELECT AVG(total) FROM orders", // average spend per order
-                "SELECT COUNT(*) FROM products WHERE name='Grain Bowl'" // number of grain bowls sold
+                "SELECT COUNT(*) FROM products WHERE name='Grain Bowl'", // number of grain bowls sold
+                "SELECT AVG(cardinality) FROM (SELECT cardinality(itemList) FROM products) AS size", //Average amount of items per product
+                "SELECT * FROM item WHERE quantity < 100", //Returns table of items that are below an arbitrary amount
+                "SELECT COUNT(*) FROM orders WHERE discount > 0", //Returns the amount of orders that used a discount
 
         };
         db.connect();
-        for (int i = 0; i < cmds.length; i++) {
+        for(int i=0;i<cmds.length;i++){
             String cmd = cmds[i];
             System.out.println("Executing command: " + cmd);
-            try {
+            try{
                 db.printResultSet(db.sendCommand(cmd));
-            } catch (Exception e) {
+            }
+            catch(Exception e){
                 e.printStackTrace();
             }
         }
