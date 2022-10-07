e6516d2a65b0c18be0749d4d9ea7fcc1c3b878d8
Kevin Champagne <kevin.champagne@tamu.edu>
2022-10-07 09:31:03 -0500

    added GUI

---
+++ GUI.java
@@ -0,0 +1,72 @@
+import java.sql.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/*
+  TODO:
+  1) Change credentials for your own team's database
+  2) Change SQL command to a relevant query that retrieves a small amount of data
+  3) Create a JTextArea object using the queried data
+  4) Add the new object to the JPanel p
+*/
+
+public class GUI extends JFrame implements ActionListener {
+    static JFrame f;
+
+    public static void initialize(dbConnection db)
+    {
+      //TODO STEP 1
+      db.connect();
+      JOptionPane.showMessageDialog(null,"Opened database successfully");
+
+      String name = "";
+      try{
+        //send statement to DBMS
+        ResultSet result = db.sendCommand("SELECT * FROM item");
+        while (result.next()) {
+          name += result.getString("name")+"\n";
+        }
+      } catch (Exception e){
+          e.printStackTrace();
+          JOptionPane.showMessageDialog(null,"Error accessing Database.");
+      }
+      // create a new frame
+      f = new JFrame("DB GUI");
+
+      // create a object
+      GUI s = new GUI();
+
+      // create a panel
+      JPanel p = new JPanel();
+
+      JButton b = new JButton("Close");
+
+      // add actionlistener to button
+      b.addActionListener(s);
+
+      //TODO Step 3
+
+      //TODO Step 4
+
+      // add button to panel
+      p.add(b);
+
+      // add panel to frame
+      f.add(p);
+
+      // set the size of frame
+      f.setSize(400, 400);
+
+      f.show();
+
+    }
+
+    // if button is pressed
+    public void actionPerformed(ActionEvent e)
+    {
+        String s = e.getActionCommand();
+        if (s.equals("Close")) {
+            f.dispose();
+        }
+    }
+}
--- jdbcpostgreSQL.java
+++ jdbcpostgreSQL.java
@@ -18,12 +18,17 @@ public class jdbcpostgreSQL {
 
     public static void main(String args[]) {
         dbConnection db = new dbConnection();
-        //List of query commands to be run through the database
+        // List of query commands to be run through the database
+        GUI.initialize(db);
+        db.close();
+    }
+
+    public static void queryTest(dbConnection db){
         String[] cmds = {
                 "SELECT SUM(total) FROM orders", // total sales
                 "SELECT AVG(total) FROM orders", // average spend per order
                 "SELECT COUNT(*) FROM products WHERE name='Grain Bowl'", // number of grain bowls sold
-                
+
                 "SELECT AVG(cardinality) FROM (SELECT cardinality(itemList) FROM products) AS size", //Average amount of items per product
                 "SELECT * FROM item WHERE quantity < 100", //Returns table of items that are below an arbitrary amount
                 "SELECT COUNT(*) FROM orders WHERE discount > 0", //Returns the amount of orders that used a discount
@@ -32,14 +37,14 @@ public class jdbcpostgreSQL {
                 "SELECT AVG(total) FROM orders GROUP BY id",  //  the average total group by id
                 "SELECT COUNT(*) FROM item WHERE quantity > 430",   // number of quantity of food greater than 430
 
-                "SELECT MAX(total) FROM orders", // finds the max someone has payed for an order 
+                "SELECT MAX(total) FROM orders", // finds the max someone has payed for an order
                 "SELECT COUNT(productlist) FROM orders WHERE cardinality(productlist) = 1", // keeps return how many orders that contain one product
                 "SELECT COUNT(orders) FROM orders WHERE date='2022-08-24'", // returns how many orders that were made on a given date, in this case 8/24/22, can be used to show game days have more sales
-                    
-                "SELECT COUNT(orders) FROM orders WHERE date='2022-08-23'", // returns sales of a specific day which is a  regular day 
-                "SELECT MAX(subtotal) from orders", // find the max orders for subtotal price 
+
+                "SELECT COUNT(orders) FROM orders WHERE date='2022-08-23'", // returns sales of a specific day which is a  regular day
+                "SELECT MAX(subtotal) from orders", // find the max orders for subtotal price
                 "SELECT COUNT(*) FROM item WHERE quantity > 200" //find number of quantitity of food greater than 200
-                 
+
 
         };
         db.connect();
@@ -54,6 +59,6 @@ public class jdbcpostgreSQL {
                 e.printStackTrace();
             }
         }
-        db.close();
     }
+
 }
