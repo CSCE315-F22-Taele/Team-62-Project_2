54029a8aa5bc924d73f9a2e9213e89065755ccf8
JP Pham <JPham97@tamu.edu>
2022-10-16 22:28:33 -0500

    added summary and inventory to new GUI

--- PomAndHoneyGUI.java
+++ PomAndHoneyGUI.java
@@ -1,10 +1,12 @@
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.uiDesigner.core.Spacer;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
 import java.sql.*;
 import java.lang.Math;
 
@@ -16,18 +18,19 @@ public class PomAndHoneyGUI extends JFrame {
 	private JButton btnOrders;
 	private JPanel serverPanel;
 	private JPanel summaryPanel;
-	private JPanel InventoryPanel;
 	private JPanel orderPanel;
+	private JPanel inventoryPanel;
 
 	private dbConnection db;
 
 	public PomAndHoneyGUI(dbConnection database) {
 		db = database;
 
-		setContentPane(mainPanel);
-		setSize(1500, 1000);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setVisible(true);
+		$$$setupUI$$$();
+		this.setContentPane(mainPanel);
+		this.setSize(1500, 1000);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 
 		btnServerView.addActionListener(new ActionListener() {
 			/**
@@ -66,36 +69,112 @@ public class PomAndHoneyGUI extends JFrame {
 			}
 		});
 
-		Inventory inventory = new Inventory(db);
-		JPanel verticalPanel = new JPanel(new BorderLayout());
-		InventoryPanel = new JPanel();
-		InventoryPanel .add( inventory.mainInventoryPanel(verticalPanel) );
 
 	}
 
+	/**
+	 * this is where to add our serverView, summary, inventory, orders
+	 * TODO: place custom component creation code here
+	 */
+	private void createUIComponents() {
+		InventoryComponents();
+		SummaryComponents();
+	}
+
+	private void InventoryComponents() {
+		Inventory inventory = new Inventory(db);
+		JPanel verticalPanel = new JPanel(new BorderLayout());
+
+		inventoryPanel = new JPanel();
+		inventoryPanel.add(inventory.mainInventoryPanel(verticalPanel));
+	}
+
+	private void SummaryComponents() {
+		summaryPanel = new JPanel();
+		JLabel title = new JLabel("Summary");
+		summaryPanel.add(title);
+		int id = 0;
+		String date = "";
+		int ordersToday = 0;
+		double salesToday = 0;
+		int ordersWeek = 0;
+		double salesWeek = 0;
+		try {
+			ResultSet r = db.sendCommand("SELECT MAX(id) FROM orders");
+			r.next();
+			id = r.getInt("max");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		}
+		try {
+			ResultSet r = db.sendCommand("SELECT date FROM orders WHERE id = " + id);
+			r.next();
+			date = r.getString("date");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		}
+		try {
+			ResultSet r = db.sendCommand("SELECT total FROM orders WHERE date = '" + date + "'");
+			while (r.next()) {
+				ordersToday++;
+				salesToday += r.getDouble("total");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		}
+
+		// create a new frame
+		//System.out.println(ordersToday + " " + salesToday);
+		String today = "                 TODAY                  \n Revenue: " + salesToday + "       Orders: " + ordersToday + "\n";
+		for (int i = 0; i < 7; i++) {
+			if (date != "") {
+				try {
+					ResultSet r = db.sendCommand("SELECT total FROM orders WHERE date = '" + date + "'");
+					while (r.next()) {
+						ordersWeek++;
+						salesWeek += r.getDouble("total");
+					}
+				} catch (Exception e) {
+					break;
+				}
+			}
+			try {
+				ResultSet r = db.sendCommand("SELECT date FROM orders WHERE date = (date '" + date + "' - integer '1')");
+				r.next();
+				date = r.getString("date");
+			} catch (Exception e) {
+				date = "";
+				continue;
+			}
+		}
+		String week = "                 Week                  \n Revenue: " + salesWeek + "       Orders: " + ordersWeek + "       Avg. Order: " + (double) salesWeek / ordersWeek;
+		JTextArea contents = new JTextArea(today + week);
+		contents.setEditable(false);
+		summaryPanel.add(contents);
+	}
 
 	public static void main(String[] args) {
-		// IMPORTANT NOTES!!!!!!!!
-		// must use to currently run, will prob need to add more stuff later:
-		// javac .\PomAndHoneyGUI.java .\dbConnection.java .\dbSetup.java .\Inventory.java .\GUI.java .\Product.java .\ProductDef.java .\serverView.java .\testGeneration.java .\Item.java .\addItems.java -cp ";forms_rt.jar"
 
-		// windows
-		// java -cp ".;forms_rt.jar;postgresql-42.2.8.jar" .\PomAndHoneyGUI.java
+		/*
+		 IMPORTANT NOTES!!!!!!!!
+		 must use to currently run, will prob need to add more stuff later:
+		 javac .\PomAndHoneyGUI.java .\dbConnection.java .\dbSetup.java .\Inventory.java .\GUI.java .\Product.java .\ProductDef.java .\serverView.java .\testGeneration.java .\Item.java .\addItems.java -cp ";forms_rt.jar"
 
-		// mac/linux
-		// java -cp ".;forms_rt.jar:postgresql-42.2.8.jar" .\PomAndHoneyGUI.java
+		 windows
+		 java -cp ".;forms_rt.jar;postgresql-42.2.8.jar" .\PomAndHoneyGUI.java
+
+		 mac/linux
+		 java -cp ".;forms_rt.jar:postgresql-42.2.8.jar" .\PomAndHoneyGUI.java
+		 */
 
 		dbConnection db = new dbConnection();
 		db.connect();
 		PomAndHoneyGUI mainFrame = new PomAndHoneyGUI(db);
+		mainFrame.setVisible(true);
 
-	}
-
-	{
-// GUI initializer generated by IntelliJ IDEA GUI Designer
-// >>> IMPORTANT!! <<<
-// DO NOT EDIT OR ADD ANY CODE HERE!
-		$$$setupUI$$$();
 	}
 
 	/**
@@ -106,6 +185,7 @@ public class PomAndHoneyGUI extends JFrame {
 	 * @noinspection ALL
 	 */
 	private void $$$setupUI$$$() {
+		createUIComponents();
 		mainPanel = new JPanel();
 		mainPanel.setLayout(new GridLayoutManager(3, 2, new Insets(0, 0, 0, 0), -1, -1));
 		final JPanel panel1 = new JPanel();
@@ -123,18 +203,14 @@ public class PomAndHoneyGUI extends JFrame {
 		btnOrders = new JButton();
 		btnOrders.setText("Orders");
 		panel1.add(btnOrders, new GridConstraints(0, 3, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(50, 50), null, 0, false));
-		summaryPanel = new JPanel();
-		summaryPanel.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
 		mainPanel.add(summaryPanel, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
 		serverPanel = new JPanel();
 		serverPanel.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
 		mainPanel.add(serverPanel, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
-		InventoryPanel = new JPanel();
-		InventoryPanel.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
-		mainPanel.add(InventoryPanel, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
 		orderPanel = new JPanel();
 		orderPanel.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
 		mainPanel.add(orderPanel, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+		mainPanel.add(inventoryPanel, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
 	}
 
 	/**
@@ -143,5 +219,6 @@ public class PomAndHoneyGUI extends JFrame {
 	public JComponent $$$getRootComponent$$$() {
 		return mainPanel;
 	}
+
 
 }
